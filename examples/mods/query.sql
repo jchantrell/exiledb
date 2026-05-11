ed1958aea720f6d33870721295c677022fa2c528
Joel Chantrell <me@jchantrell.dev>
2026-05-11 09:38:37 +0930

    fix: join untranslated tables on English in mods query

--- examples/mods/query.sql
+++ examples/mods/query.sql
@@ -12,13 +12,13 @@ SELECT
   s4c.name as stat4_category
 FROM mods m
 JOIN mods_families_junction mfj ON m._language = mfj._language AND m._index = mfj._parent_index
-JOIN mod_family mf ON mfj._language = mf._language AND mfj.value = mf._index
-LEFT JOIN stats s1 ON m._language = s1._language AND m.stat1 = s1._index
-LEFT JOIN passive_skill_stat_categories s1c ON s1._language = s1c._language AND s1.category = s1c._index
-LEFT JOIN stats s2 ON m._language = s2._language AND m.stat2 = s2._index
-LEFT JOIN passive_skill_stat_categories s2c ON s2._language = s2c._language AND s2.category = s2c._index
-LEFT JOIN stats s3 ON m._language = s3._language AND m.stat3 = s3._index
-LEFT JOIN passive_skill_stat_categories s3c ON s3._language = s3c._language AND s3.category = s3c._index
-LEFT JOIN stats s4 ON m._language = s4._language AND m.stat4 = s4._index
-LEFT JOIN passive_skill_stat_categories s4c ON s4._language = s4c._language AND s4.category = s4c._index
+JOIN mod_family mf ON mf._language = 'English' AND mfj.value = mf._index
+LEFT JOIN stats s1 ON s1._language = 'English' AND m.stat1 = s1._index
+LEFT JOIN passive_skill_stat_categories s1c ON s1c._language = 'English' AND s1.category = s1c._index
+LEFT JOIN stats s2 ON s2._language = 'English' AND m.stat2 = s2._index
+LEFT JOIN passive_skill_stat_categories s2c ON s2c._language = 'English' AND s2.category = s2c._index
+LEFT JOIN stats s3 ON s3._language = 'English' AND m.stat3 = s3._index
+LEFT JOIN passive_skill_stat_categories s3c ON s3c._language = 'English' AND s3.category = s3c._index
+LEFT JOIN stats s4 ON s4._language = 'English' AND m.stat4 = s4._index
+LEFT JOIN passive_skill_stat_categories s4c ON s4c._language = 'English' AND s4.category = s4c._index
 WHERE m._language = '$LANG';
